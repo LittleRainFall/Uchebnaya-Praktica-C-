022e5901a8fba178fe7f31001adfdc8669a66455
LittleRainFall <118064052+LittleRainFall@users.noreply.github.com>
2023-03-03 18:22:27 +0700

    Add files via upload

---
+++ 2.1/1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String j = Console.ReadLine();
+            String s = Console.ReadLine();
+
+            int otvet = 0;
+
+            foreach (char ch in s)
+            {
+                if (j.IndexOf(ch) >= 0)
+                {
+                    otvet++;
+                }  
+            }
+            Console.WriteLine(otvet);
+        }
+    }
+}
---
+++ 2.1/3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool function(int[] nums)
+            {
+                List<int> num = new List<int>();
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (num.Contains(nums[i]))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        num.Add(nums[i]);
+                    }
+
+                     
+                }
+                return false;
+            }
+             
+            
+            int[] nums = { 1, 2, 3, 4, 5 };
+
+            bool fun = function(nums);
+            Console.WriteLine(fun);
+
+        }
+        }
+    }
