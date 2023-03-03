3da38fb58833647ea18bd16b78d78e2fd2a23e3f
LittleRainFall <118064052+LittleRainFall@users.noreply.github.com>
2023-03-03 17:45:18 +0700

    Add files via upload

---
+++ 2.3/1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker Denis = new Worker();
+            Denis.Sasha();
+        }
+    }
+}
---
+++ 2.3/2/Worker.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace ConsoleApp6
+{
+    public class Worker
+    {
+        private string name;
+        private string surname;
+        private int rate;
+        private int day;
+    
+        private string Name
+        {
+            get => this.name;
+            set => this.name = value;
+        }
+
+        private string Surname
+        {
+            get => this.surname;
+            set => this.surname = value;
+        }
+
+        private int Rate
+        {
+            get => this.rate;
+            set => this.rate = value;
+        }
+
+        private int Day
+        {
+            get => this.day;
+            set => this.day = value;
+        }
+
+        public Worker()
+        {
+            this.Name = "Danila";
+            this.Surname = "Sukhushin";
+            this.Rate = 2190;
+            this.Day = 23;
+        }
+
+        public void Sasha()
+        {
+            Console.WriteLine($"Зарплата сотрудника {name} {surname} составляет {rate*day}");
+        }
+    }
+}
