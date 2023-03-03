a55310b2eb0d956c7545a609fc2b6359645e2767
LittleRainFall <118064052+LittleRainFall@users.noreply.github.com>
2023-03-03 17:48:32 +0700

    Add files via upload

---
+++ 2.4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ConsoleApp7
+{
+    public class Program
+    {
+
+        public static int ConvertToArabic(string roman)
+        {
+
+            Dictionary<char, int> map = new Dictionary<char, int>()
+            {
+                { 'I', 1 },
+                { 'V', 5 },
+                { 'X', 10 },
+                { 'L', 50 },
+                { 'C', 100 },
+                { 'D', 500 },
+                { 'M', 1000 }
+            };
+
+            int result = 0;
+            for(int i= 0; i<roman.Length;
+                i++)
+            {
+                if (i + 1 < roman.Length && map[roman[i]] < map[roman[i + 1]])
+                {
+                    result -= map[roman[i]];
+                }
+                else
+                {
+                    result += map[roman[i]];
+                }
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            int arabicNumber;
+            Console.Write("Enter roman number: ");
+            string roman=Console.ReadLine();
+            arabicNumber = ConvertToArabic(roman);
+            Console.WriteLine("Arabic number - " + arabicNumber);
+            
+ 
+        }
+    }
+}
